1a0c01405887c6d6ecdbe9daafe378d483fb1774
truongviet2k3 <truongviet2k3@gmail.com>
2022-06-16 10:19:51 +0700

    Update

---
+++ C/300 CODE C/Bai21.c
@@ -0,0 +1,46 @@
+// Input: Diem chuan hoi dong, diem 3 mon thi, khu vuc cua thi sinh, doi tuong du thi
+// Output: Do hay truot
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float diem_chuan, mon1, mon2 , mon3, tong_diem;
+    char kv;
+    int doi_tuong;
+    printf("Nhap diem chuan: "); scanf("%f",&diem_chuan);
+    printf("Nhap diem 3 mon thi: "); scanf("%f %f %f" , &mon1, &mon2, &mon3);
+    printf("Nhap khu vuc (A, B, C, X): "); scanf("%c", &kv);
+    printf("Nhap doi tuong (1, 2, 3, 0): "); scanf("%d", &doi_tuong);
+    tong_diem = mon1 + mon2 + mon3;
+    switch (kv)
+    {
+    case 'A':
+        tong_diem += 2;
+        break;
+    case 'B':
+        tong_diem += 1;
+        break;
+    case 'C':
+        tong_diem += 0.5;
+        break;
+    default:
+        break;
+    }
+    switch (doi_tuong)
+    {
+    case 1:
+        tong_diem += 2.5;
+        break;
+    case 2:
+        tong_diem += 1.5;
+        break;
+    case 3:
+        tong_diem += 1;
+        break;
+    default:
+        break;
+    }
+    (tong_diem >= diem_chuan) ? printf("Do [%.1f]", tong_diem) : printf("Rot [%.1f]", tong_diem); 
+    return 0;
+}
