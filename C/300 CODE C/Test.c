cfcd58a6c2e6caae3e7335900149e07b26e4b4bb
unknown <truongviet2k3@gmail.com>
2022-06-25 16:26:37 +0700

    Update

--- C/300 CODE C/Test.c
+++ C/300 CODE C/Test.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
+
 int main() {
- unsigned cel, fah;
- printf( "%10s%14s\n", "Celcius", "Fahrenheit" );
- for ( cel = 0; cel <= 10; ++cel )
- printf( "%10u%14.2f\n", cel, 9.0 * cel / 5 + 32 );
- printf( "\n%10s%14s\n", "Fahrenheit", "Celcius" );
- for ( fah = 32; fah <= 42; ++fah )
- printf( "%10u%14.2f\n", fah, 5.0 * ( fah - 32 ) / 9 );
- return 0;
+    int n, k, count = 0;
+    int score[51];
+    scanf("%d %d", &n, &k);
+    int i;
+    for(i = 0; i < n; i++) {
+        scanf("%d", &score[i]);
+    }  
+    i = 0;
+    while (k > 1 && i < n) 
+    {
+        if(score[i] > 0 ) {
+            count ++;
+            k--;
+            i++;
+        }
+        else {
+            printf("%d", count);
+            return 0;
+        };
+    }
+    printf("%d\n", i);
+    while(k == 1) {
+        if(score[i] > 0 && score[i] > score[i+1]) {
+            count ++;
+            k --;
+        }
+        else{
+            for(;i < n; i++) {
+                if(score[i] == score[i+1] && score[i] > 0) {
+                count ++;
+            }
+        } 
+        
+    }
+    }
+   
+    printf("%d", count);
+    return 0;
 }
